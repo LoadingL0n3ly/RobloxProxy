4b0d05906054750f1163644feefbb3b3413b80ac
LoadingL0n3ly <fedor.khaldin@gmail.com>
2023-07-14 08:17:42 -0700

    setup

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express")
+var request = require("request")
+var app = express()
+var port = 80
+
+app.use(express.json())
+
+app.post("/ProxyGet", function(Request, Res){
+    if (!Request.body.url){
+        Res.send("No url provided")
+        return
+    }
+
+    var URL = Request.body.url
+    request(URL).pipe(Res)
+
+})
+
+app.listen(port)
